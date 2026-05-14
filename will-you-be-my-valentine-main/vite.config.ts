905b67f596a4b2157d2f35815e98b6c39e5a6f27
XPMatth <matheusarrudasilva18@gmail.com>
2026-05-13 23:58:40 -0300

    build: add docs for GitHub Pages

--- will-you-be-my-valentine-main/vite.config.ts
+++ will-you-be-my-valentine-main/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/cutRepository/',
+  base: '/cuteRepository/',
+  build: {
+    outDir: 'docs',
+  },
 })
